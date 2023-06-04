179470e44041c5bce6d2581f469eb37938afa36b
mbps54 <61657439+mbps54@users.noreply.github.com>
2023-06-04 17:10:01 +0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,10 @@
+!# /bin/bash
+sudo yum install -y yum-utils
+sudo yum install git -y
+sudo yum-config-manager \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+
+sudo systemctl start docker
+sudo docker run hello-world
